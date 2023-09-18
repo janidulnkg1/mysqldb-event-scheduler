e587952371d095b9a02a06ae348bdacadc4cd393
janidulnkg1 <janidulnkg1@gmail.com>
2023-09-18 16:37:02 +0530

    add changes

--- Database Event Sheduler.sql
+++ Database Event Sheduler.sql
@@ -3,7 +3,7 @@ SET GLOBAL event_scheduler = ON;
 DELIMITER //
 CREATE PROCEDURE RemoveOldData()
 BEGIN
-   DECLARE cutoff_date DATETIME;
+    DECLARE cutoff_date DATETIME;
     SET cutoff_date = DATE_SUB(NOW(), INTERVAL 2 MONTH);
     
     DECLARE done INT DEFAULT FALSE;
@@ -19,36 +19,36 @@ BEGIN
             LEAVE db_loop;
         END IF;
     
-		DECLARE done_tables INT DEFAULT FALSE;
-		DECLARE table_name VARCHAR(255);
-		DECLARE cur_tables CURSOR FOR
-			SELECT table_name
-			FROM information_schema.tables
-			WHERE table_schema = db_name;
-		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done_tables = TRUE;
+        DECLARE done_tables INT DEFAULT FALSE;
+        DECLARE table_name VARCHAR(255);
+        DECLARE cur_tables CURSOR FOR
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = db_name;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done_tables = TRUE;
         
-		OPEN cur_tables;
+        OPEN cur_tables;
         
-		table_loop: LOOP
-			FETCH cur_tables INTO table_name;
-			IF done_tables THEN
-				LEAVE table_loop;
-			END IF;
+        table_loop: LOOP
+            FETCH cur_tables INTO table_name;
+            IF done_tables THEN
+                LEAVE table_loop;
+            END IF;
     
-			SET @sql = CONCAT('DELETE FROM ', db_name, '.', table_name, ' WHERE DATET < "', DATE_FORMAT(cutoff_date, '%Y-%m-%d %H:%i:%s'), '"');
+            SET @sql = CONCAT('DELETE FROM ', db_name, '.', table_name, ' WHERE DATET < "', DATE_FORMAT(cutoff_date, '%Y-%m-%d %H:%i:%s'), '"');
             
-			PREPARE stmt FROM @sql;
-			EXECUTE stmt;
-			DEALLOCATE PREPARE stmt;
-		END LOOP;
+            PREPARE stmt FROM @sql;
+            EXECUTE stmt;
+            DEALLOCATE PREPARE stmt;
+        END LOOP;
         
-		CLOSE cur_tables;
-	END LOOP;
+        CLOSE cur_tables;
+    END LOOP;
     
     CLOSE cur;
     
-END 
-
+END //
+DELIMITER ;
 
 CREATE EVENT RemoveOldDataEvent
 ON SCHEDULE EVERY 1 DAY
